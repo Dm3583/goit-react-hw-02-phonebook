2509fd425fdf405c993c219919fadc01a3f1f655
Dm3583 <pankovdm@gmail.com>
2021-03-01 17:03:14 +0200

    base markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Phone book</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      name: "",
+    };
+  }
+
+  inputFieldHandler = (evt) => {
+    console.log(evt.target.value);
+    return this.setState({
+      name: evt.target.value,
+    });
+  };
+
+  btnOnclickHandler = (evt) => {
+    evt.preventDefault();
+    console.log(this.state.name);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            <input type="text" name="name" onChange={this.inputFieldHandler} />
+          </label>
+          <button type="button" onClick={this.btnOnclickHandler}>
+            Add contact
+          </button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li>Contact 1</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
